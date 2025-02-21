54be692de777ec91c80b1c32c0fbafcb188f9583
[Karthik Kagolanu] <[sai.kagolanu@yahoo.com]>
2025-02-21 15:20:08 +0000

    added images for sky forecast and fixed temperature display with exception handling.

--- backend.py
+++ backend.py
@@ -3,15 +3,17 @@ import requests
 API_KEY = "26682c8aab4129923404f16078aecf00"
 
 
-def get_data(place, forcast_days=None, kind=None):
+def get_data(place, forecast_days=None):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
+    if "list" not in data:
+        return None
     filtered_data = data["list"]
-    nr_values = 8*forcast_days
+    nr_values = 8*forecast_days
     filtered_data = filtered_data[:nr_values]
-    return data
+    return filtered_data
 
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo"))
+    print(get_data(place="Tokyo",forecast_days=3))
